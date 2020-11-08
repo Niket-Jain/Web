672df15e92a651a6fa5ea8f663fbbbfd148f060f
Niket-Jain <52085669+Niket-Jain@users.noreply.github.com>
2020-11-08 23:55:06 +0530

    Keeper Basic Setup

---
+++ Keeper/src/components/Note.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Mikhail Tal</h1>
+      <p>Magician from Riga was born on 12.09</p>
+    </div>
+  );
+}
+
+export default Note;
