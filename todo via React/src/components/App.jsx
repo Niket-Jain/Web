2ba91dfb61040b4549e7d9b995d7e454f504755f
Suvansh Rana <36293610+suvansh-rana@users.noreply.github.com>
2020-11-13 12:17:48 +0530

    Added delete & delete all feature, fixed bugs, added features

--- todo via React/src/components/App.jsx
+++ todo via React/src/components/App.jsx
@@ -1,9 +1,8 @@
 import React, { useState } from "react";
 
 function App() {
-  
   // State for a single line Input
-  const [text, setText] = useState("add items");
+  // const [text, setText] = useState("");
 
   // State for saving the items in an Array.
   const [itemList, setItemList] = useState(
@@ -11,37 +10,105 @@ function App() {
     []
   );
 
-  function textHandle(event) {
-    const newValue = event.target.value;
-    setText(newValue);
+  // function textHandle(event) {
+  //   setText(event.target.value);
+  // }
+
+  // function addItem() {
+  //   // Check whether the text is empty or not
+  //   if (text.length > 0) {
+  //     setItemList((prevItemList) => {
+  //       // ...prevItemList is a spread operator of JS6
+  //       return [...prevItemList, text];
+  //     });
+
+  //     setText("");
+  //   }
+  // }
+
+  function emptyTodo() {
+    // Empty the todo list
+    setItemList([]);
   }
 
-  function addItem() {
-    setItemList((prev) => {
-      // ...prev is a spread operator of JS6
-      return [...prev, text];
-    });
+  // Delete a specific item in the array
+  function deleteTodo(index) {
 
-    setText("");
+    // Create a copy of the orginal list
+    let _itemList = [...itemList];
+
+    // Delete the item at the desired index
+    _itemList.splice(index, 1);
+
+    // Set the new array as `itemList`
+    setItemList(_itemList);
+  }
+
+  function handleSubmit(event) {
+    // Prevent the default behaviour of form submit
+    // After form submission, the user will be redirected to the
+    // `action` url provided in the form, but we don't want that.
+    // Basically, prevent the form to reload the page
+    event.preventDefault();
+
+    // Accessing the form
+    const form = event.target;
+
+    // Accessing the input field
+    // using the `name` atrribute provided in the html
+    const inputField = form.elements["listItem"];
+
+    // Accessing the value of input field and removing trailing spaces
+    const inputText = inputField.value.trim();
+
+    // Check whether the text is empty or not
+    if (inputText.length > 0) {
+      // Adding todo
+
+      // Adding items at front of the array,
+      // so that newly added items remains at the top
+      setItemList((prevItemList) => [inputText, ...prevItemList]);
+
+      // Reseting the input field
+    }
+    inputField.value = "";
   }
 
   return (
     <div className="container">
+
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input name="listItem" onChange={textHandle} type="text" value={text} />
-        <button onClick={addItem}>
+
+      <form className="form" onSubmit={handleSubmit}>
+        <input
+          name="listItem"
+          placeholder="Add items..."
+          // onChange={textHandle}
+          type="text"
+          // value={text}
+        />
+
+        <button type="submit">
           <span>Add</span>
         </button>
-      </div>
+      </form>
+
       <div>
         <ul>
-          {itemList.map((item) => (
-            <li> {item} </li>
+          {itemList.map((item, index) => (
+            <li key={"item-" + index}>
+              {item}
+              <span onClick={() => deleteTodo(index)}>X</span>
+            </li>
           ))}
         </ul>
+        {itemList.length > 0 ? (
+          <button onClick={emptyTodo}>
+            <span>Delete all</span>
+          </button>
+        ) : null}
       </div>
     </div>
   );
